01dc2af0f5aa85dfee272a43a37b4d13f21491f4
AlyxMoon <allisterkmoon@gmail.com>
2021-07-30 08:21:59 -0700

    initial commit, basic framework setup

---
+++ Salon/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Salon.Controllers
+{
+  [Route("")]
+  public class HomeController : Controller
+  {
+    [HttpGet]
+    public ActionResult Index ()
+    {
+      return View();
+    }
+  }
+}
